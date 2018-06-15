8bebc7a923f7394432ac9dd05a711ddc95328ebc
Mikel López <mlvillarroya@gmail.com>
2018-06-15 19:27:33 +0200

    Módulo Library para Odoo. Primera subida.

---
+++ odoo-10.0.post20180323/odoo/addons/library/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "library",
+
+    'summary': """
+        Odoo module for a library""",
+
+    'description': """
+        Odoo module for a library
+        by Mikel López
+    """,
+
+    'author': "Mikel López",
+    'website': "http://github.com/mlvillarroya",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
