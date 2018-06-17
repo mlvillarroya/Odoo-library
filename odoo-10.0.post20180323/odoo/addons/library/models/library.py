195e031c141cc58ef7fa302a5eb92a16ff74e319
Mikel López <mlvillarroya@gmail.com>
2018-06-17 19:22:36 +0200

    Módulo Library para Odoo. Creada clase member Creada vista form para member

--- odoo-10.0.post20180323/odoo/addons/library/models/library.py
+++ odoo-10.0.post20180323/odoo/addons/library/models/library.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, timedelta
 
-# class library(models.Model):
-#     _name = 'library.library'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class member(models.Model):
+    # Maquinas objeto del librarymiento"""
+    _name = 'library.member'
+
+    name = fields.Char(size=32, string='Member name', index=True)
+    id_number = fields.Char(size=32, string='DNI/NIE', index=True)
+    membership_number = fields.Char('Membership no')
+    date_membership = fields.Date(string='Date membership', default=fields.Datetime.now)
+    active = fields.Boolean('Active', default=True)
+    email = fields.Char(string='Email')
+    phone = fields.Char(string='Phone')
+    address = fields.Char(size=32, string='Adress')
+    city = fields.Char(size=32, string='City')
+    postalcode = fields.Char('Postal code')
+
+    _sql_constraints = [('DNI_uniq', 'unique (id_number)', "DNI/NIE already exists !")]
